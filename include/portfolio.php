0dd20400078536ee9946d253a25ef685b798eafd
Wendy Chew <wendychew@Wendys-MacBook-Pro.local>
2024-07-26 15:31:50 -0700

    added the missing files in the include folder

---
+++ include/portfolio.php
@@ -0,0 +1,284 @@
+<section class="main2" id="portfolio">
+    <div class="container container-shadow">
+        <h1>Projects</h1>
+        <!-- Grid row -->
+        <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-lg-4 col-md-12 mb-4">
+            <!--Modal: Name-->
+            <div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+        
+            <!--Content-->
+            <div class="modal-content">
+            <!--Body-->
+            <div class="modal-body mb-0 p-0">
+                <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/iuNUfnmRFKo"
+                    allowfullscreen></iframe>
+                </div>
+                <div class="p-3 m-0 project-text">
+                    <h5 class="text-uppercase font-weight-bold">Chew's </h5>
+                    <hr class="project-hr">
+                        <p>My first web design for an e-commerce site using pure HTML, CSS, JavaScript and JQuery</p>
+                        <dl>
+                            <dt>Type :</dt>
+                            <dd>E-commerce Site</dd>
+                            <dd><a href="http://myweb.students.wwu.edu/~cheww/CS102/WCVS/webproject/index.html" target=”_blank” class="btn btn-project"><i class="fas fa-window-restore mr-2" ></i> View Project</a>
+                            </dd>
+                        </dl>
+                </div>
+            </div>  
+            <!--Footer-->
+            <div class="modal-footer justify-content-center">
+            <button type="button" class="btn button2 btn-rounded btn-md " data-dismiss="modal">Close</button>
+
+            </div>
+    
+            </div>
+            <!--/.Content-->
+                </div>
+            </div>
+
+        <!--image-->
+        <a><img class="img-fluid z-depth-1 shadow img" src="image/project1.jpeg" alt="" data-toggle="modal" data-target="#modal1"></a>  
+        </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-lg-4 col-md-6 mb-4">
+        <!--Modal: Name-->
+        <div class="modal fade" id="modal6" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+        <!--Content-->
+        <div class="modal-content">
+
+        <!--Body-->
+        <div class="modal-body mb-0 p-0">
+            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/UcOiFWxBhG0"
+                allowfullscreen></iframe>
+            </div>                
+            <div class="p-3 m-0 project-text">
+            <h5 class="text-uppercase font-weight-bold">My Portfolio</h5>
+            <hr class="project-hr">
+            <ul>
+                <li><p>A dynamic website application developed with several functionalities, showing the skills and experience of me</p></li>
+                <li><p>Project developed using HTML, CSS, JavaScript, Jquery, MySQL and PHP</p></li>
+            </ul>
+                <dl>
+                    <dt>Type :</dt>
+                    <dd>Portfolio Site</dd>
+                    <dt>Link : </dt>
+                    <dd>https://wendychew.me</dd>
+                </dl>
+            </div>
+        </div>
+        <!--Footer-->
+        <div class="modal-footer justify-content-center">
+            <button type="button" class="btn button2 btn-rounded btn-md " data-dismiss="modal">Close</button>
+        </div>
+        </div>
+        <!--/.Content-->
+        </div>
+        </div>
+        <!--image-->
+        <a><img class="img-fluid z-depth-1 shadow img" src="image/project2.jpeg" alt=""
+        data-toggle="modal" data-target="#modal6"></a>
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-lg-4 col-md-6 mb-4">
+
+        <!--Modal: Name-->
+        <div class="modal fade" id="modal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+
+        <!--Content-->
+        <div class="modal-content">
+
+            <!--Body-->
+            <div class="modal-body mb-0 p-0">
+            <div class="embed-responsive z-depth-1-half">
+                <img class="img-fluid z-depth-1 shadow" src="image/lab202.jpg" alt="">
+            </div>
+            <div class="p-3 m-0 project-text">
+                <h5 class="text-uppercase font-weight-bold">Bellingham Hotel</h5>
+                <hr class="project-hr">
+                <ul>
+                <li><p>A dynamic hotel booking website with contact form and reservation form</p></li>
+                <li> <p>Project developed using HTML, CSS, JavaScript, Jquery and PHP </p> </li>
+                </ul>
+                <dl>
+                    <dt>Type :</dt>
+                    <dd>Booking Site</dd>
+                    <dd><a href="https://studentweb.cs.wwu.edu/~cheww/CS202/WCVS/Lab6/index.html" target=”_blank” class="btn btn-project"><i class="fas fa-window-restore mr-2" ></i> View Project</a>
+                    </dd>
+                </dl>
+            </div>
+            </div>
+
+            <!--Footer-->
+            <div class="modal-footer justify-content-center">
+                <button type="button" class="btn button2 btn-rounded btn-md " data-dismiss="modal">Close</button>
+            </div>
+            </div>
+            <!--/.Content-->
+        </div>
+        </div>
+        <!--image-->
+        <a><img class="img-fluid z-depth-1 shadow img" src="image/lab202.jpg" alt="video" data-toggle="modal" data-target="#modal4"></a>
+    </div>
+    <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+    
+    <!-- Second row -->
+    <div class="row">
+        <!-- Grid column -->
+        <div class="col-lg-4 col-md-12 mb-4">
+            <!--Modal: Name-->
+            <div class="modal fade" id="modal7" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+
+            <!--Content-->
+            <div class="modal-content">
+            <!--Body-->
+            <div class="modal-body mb-0 p-0">
+                <div class="embed-responsive z-depth-1-half">
+                    <img class="img-fluid z-depth-1 shadow" src="image/wacefeProject.png" alt="">
+                </div>
+                <div class="p-3 m-0 project-text">
+                    <h5 class="text-uppercase font-weight-bold">Wacefe </h5>
+                    <hr class="project-hr">
+                        <ul>
+                            <li><p>A website that allows WACEFE to reach a larger audience and aggregate their information into one platform </p></li>
+                            <li><p>Fulfilled the team's specification using HTML, CSS, JavaScript and Squarespace</p></li>
+                        </ul>
+                
+                        <dl>
+                            <dt>Client :</dt>
+                            <dd>Washington Council for Economic and Financial Education (WACEFE)</dd>
+
+                            <dt>Type :</dt>
+                            <dd>Organization Site</dd>
+                            <dd><a href="https://wacefe.org" target=”_blank” class="btn btn-project"><i class="fas fa-window-restore mr-2" ></i> View Project</a>
+                            </dd>
+                        </dl>
+                </div>
+            </div>  
+            <!--Footer-->
+            <div class="modal-footer justify-content-center">
+            <button type="button" class="btn button2 btn-rounded btn-md " data-dismiss="modal">Close</button>
+
+            </div>
+
+            </div>
+            <!--/.Content-->
+                </div>
+            </div>
+
+        <!--image-->
+        <a><img class="img-fluid z-depth-1 shadow img" src="image/wacefeProject.png" style="filter: brightness(0.8)" alt="" data-toggle="modal" data-target="#modal7"></a>  
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-lg-4 col-md-6 mb-4">
+        <!--Modal: Name-->
+        <div class="modal fade" id="modal8" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+        <!--Content-->
+        <div class="modal-content">
+
+        <!--Body-->
+        <div class="modal-body mb-0 p-0">
+            <div class="embed-responsive z-depth-1-half">
+                    <img class="img-fluid z-depth-1 shadow" src="image/paperwork.png" alt="">
+            </div>          
+            <div class="p-3 m-0 project-text">
+            <h5 class="text-uppercase font-weight-bold">Paperwork </h5>
+            <hr class="project-hr">
+                <ul>
+                    <li><p>A website that allows Washington Civil Air Patrol members to choose and fill out forms, access them online through the website, and send forms to other members to get them authorized</p></li>
+                    <li><p>Project developed using HTML, CSS, JavaScript, PHP, MySQL and Google API</p></li>
+                </ul>
+                <dl>
+                    <dt>Client :</dt>
+                    <dd>Aran Clauson</dd>
+                    <dt>Type :</dt>
+                    <dd>Form Approving & Form Sending Site</dd>
+                    <dd><a href="https://pcr-wa-015.org/paper/home.php" target=”_blank” class="btn btn-project"><i class="fas fa-window-restore mr-2" ></i> View Project</a>
+                    </dd>
+                </dl>
+            </div>
+        </div>
+        <!--Footer-->
+        <div class="modal-footer justify-content-center">
+            <button type="button" class="btn button2 btn-rounded btn-md " data-dismiss="modal">Close</button>
+        </div>
+        </div>
+        <!--/.Content-->
+        </div>
+        </div>
+        <!--Modal: Name-->
+        <a><img class="img-fluid z-depth-1 shadow img" src="image/paperwork.png" style="filter: brightness(0.8)" alt=""
+        data-toggle="modal" data-target="#modal8"></a>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-lg-4 col-md-6 mb-4">
+
+        <!--Modal: Name-->
+        <div class="modal fade" id="modal9" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+
+        <!--Content-->
+        <div class="modal-content">
+
+            <!--Body-->
+            <div class="modal-body mb-0 p-0">
+            <div class="embed-responsive z-depth-1-half">
+                <img class="img-fluid z-depth-1 shadow" src="image/fridgeApp.JPEG" alt="">
+            </div>
+            <div class="p-3 m-0 project-text">
+                <h5 class="text-uppercase font-weight-bold">What's In Your Fridge </h5>
+                <hr class="project-hr">
+                <ul>
+                    
+                    <li><p>A recipe generator mobile application based on the users' ingredients in their fridge </p></li>
+                    <li><p>Project developed using kotlin and Android Studio</p></li>
+                </ul>
+                <dl>
+                    <dt>Type :</dt>
+                    <dd>Android Mobile Application</dd>
+                    <dd><a href="https://github.com/wendy-chew/whats-in-your-fridge" target=”_blank” class="btn btn-project"><i class="fas fa-window-restore mr-2" ></i> View Project Repository </a>
+                    </dd>
+                </dl>
+            </div>
+            </div>
+
+            <!--Footer-->
+            <div class="modal-footer justify-content-center">
+                <button type="button" class="btn button2 btn-rounded btn-md " data-dismiss="modal">Close</button>
+            </div>
+            </div>
+            <!--/.Content-->
+        </div>
+        </div>
+        <!--Modal: Name-->
+        <a><img class="img-fluid z-depth-1 shadow img" src="image/fridgeApp.JPEG" style="filter: brightness(0.8)" alt="video" data-toggle="modal" data-target="#modal9"></a>
+        </div>
+        <!-- Grid column -->
+
+        </div>
+        <!-- Second row -->
+
+
+
+</section>
